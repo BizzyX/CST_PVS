86bf46c2f0294ae05606d65b67ee04a046b6fc87
BizzyX <33569868+BizzyX@users.noreply.github.com>
2018-08-21 12:18:56 +0200

    Hamming Distance and random number

--- CST_PVS/cst_parallel.c
+++ CST_PVS/cst_parallel.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <mpi.h>
+#include <time.h>
 
 #ifdef _MSC_VER
 #define _CRT_SECURE_NO_WARNINGS
@@ -10,6 +11,8 @@
 #define BUFF_SIZE 1000
 
 void testGetFile();
+char randomStringNumber();
+
 
 
 int main(int argc, char* argv[])
@@ -19,40 +22,12 @@ int main(int argc, char* argv[])
 	//Initialize the MPI environment
 	MPI_Init(NULL, NULL);
 
-	//read from .txt file 'strings.txt'
-	//FILE *ptr_strings = (fopen_s(&ptr_strings, "strings.txt", "r"));
 	char buf[BUFF_SIZE];
 	int numberArray[BUFF_SIZE];
 	int i;
 
-	/*
-	//read unsuccesful
-	if (!ptr_strings)
-	{
-		printf("File Cannot Be Opened!\n");
-		return 1;
-	}
-	//read succesful
-	if (ptr_strings)
-	{
 
-		printf("File was succesfully openend!\n");
-		
-		//EXCEPTION
-		/*
-		for ( i = 0; i < BUFF_SIZE; i++)
-		{
-			fscanf(ptr_strings, "%1d", &numberArray[i]);
-		}
-		for ( i = 0; i < BUFF_SIZE; i++)
-		{
-			printf("Number is: %d\n\n", numberArray[i]);
-		}
-		
-	
-}
-*/
-
+	//----- MPI FUNCTIONS --------
 	//Get the number of processes
 	int world_size;
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
@@ -68,7 +43,18 @@ int main(int argc, char* argv[])
 
 	//Finalize the MPI Environment
 	MPI_Finalize();
+
 	testGetFile();
+
+	//Testvariables
+	char str1[] = "123456";
+	char str2[] = "122456";
+	int Lenght = 6;
+
+	//randomStringNumber(Lenght);
+
+
+	hammingDistance(str1,str2);
 
 	system("pause");
 	return 0;
@@ -76,35 +62,96 @@ int main(int argc, char* argv[])
 }
 
 void testGetFile() {
+	
 	// open file
 	FILE *fp = fopen("strings.txt", "r");
 	size_t len = 255;
+	
 	int arraySize = 0;
+	int arrayLenght = 0;
 	void** stringArray;
+	
 	// need malloc memory for line, if not, segmentation fault error will occurred.
 	char *line = malloc(sizeof(char) * len);
+	
 	// check if file exist (and you can open it) or not
 	if (fp == NULL) {
-		printf("can't open file input1.txt!");
+		printf("can't open file strings.txt!");
 		return;
 	}
 
+	//read the first line - write it into the array size
 	if (fgets(line, len, fp) != NULL) {
 	
 		arraySize = atoi(line);
-		printf("%s\n", line);
-		//printf("test");
-	
+		printf("Size: %s\n", line);
+		
 	}
 
+	//read the second line - write it into the arrayLenght
+	if (fgets(line, len, fp) != NULL) {
+
+		arrayLenght = atoi(line);
+		printf("Lenght: %s\n", line);
+
+	}
+	printf("Content of Array: \n\n");
 	while (fgets(line, len, fp) != NULL) {
 		int count = 0;
-		//printf("%d\n", arraySize);
 		stringArray = malloc(arraySize * sizeof * stringArray);
 		stringArray[count] = line; 
-		//printf("%d\n", sizeof(void*));
 		printf("%s\n", stringArray[count]);
 		count++;
 	}
+	printf("\n");
+	
 	free(line);
 }
+
+int hammingDistance(char *str1, char *str2)
+{
+	int i = 0, count = 0;
+	while (str1[i] != NULL)
+	{
+		
+		if (str1[i] != str2[i])
+		{
+			count++;
+		}
+		i++;
+	}
+	printf("the hamming distance is: %d \n", count);
+	
+	return count;
+	
+}
+
+
+/*
+char randomStringNumber(int *Lenght) 
+{
+	int numberLenght = Lenght;
+	printf("numberLenght:%d\n", numberLenght);
+	srand(time(NULL));
+	
+	//malloc for string lenght
+	char *stringLenght = malloc(sizeof(char) * numberLenght);
+	//array for random string
+	void** stringArray;
+	stringArray = malloc(numberLenght * sizeof *stringArray);
+	printf("stringArray: %d", stringArray);
+
+	
+	
+
+	for (int i = 1; i <= numberLenght; i++)
+	{
+		
+		int randomNumber = (rand() % numberLenght + 1);
+		stringArray[i] = randomNumber + '0';
+		printf("%d\n", stringArray[i]);
+
+	}
+	printf("Stringnumber: %d\n", stringArray);
+}
+*/
