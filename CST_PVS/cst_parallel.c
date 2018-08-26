56ec1ccb9e10ca0b473e1c1c2b685670d19ad2ab
BizzyX <33569868+BizzyX@users.noreply.github.com>
2018-08-26 23:45:11 +0200

    printfunction to save the time results in a .txt file

--- CST_PVS/cst_parallel.c
+++ CST_PVS/cst_parallel.c
@@ -9,8 +9,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #endif
 
-//#define BUFF_SIZE 1000
-
 typedef struct stringArray_
 {
 	void** data;
@@ -19,19 +17,16 @@ typedef struct stringArray_
 
 } stringArray;
 
+//initialize the functions
 stringArray testGetFile();
 char randomStringNumber();
 char *generateString();
 void printResults();
+//void printTimeToTxt();
 
 
 /*
-	TODO: 
-			- Funktion um einen Random String der 6 Zeichen lang ist und nur Zahlen im Bereich 0-6 enthält
-			- Es wird übergeben beim Programmaufruf, wie viele Strings eingelesen werden sollen
-			- Verbosity übergeben
-			- Printfunktion
-
+			Original-Text-File
 			Values:
 			6
 			6
@@ -50,28 +45,23 @@ int main(int argc, char **argv)
 	//Initialize the MPI environment
 	MPI_Init(&argc, &argv);
 
+	//Variables
 	clock_t begin, end;
 	int stringsToRead = atoi(argv[1]);
 	int verbosity = atoi(argv[2]);
-
 	int myrank, nprocs;
 	int namelen;
 	char name[MPI_MAX_PROCESSOR_NAME];
-
-	//int *sizeLength = malloc(2 * sizeof(int));
 	
-
-	//char buf[BUFF_SIZE];
-	//int numberArray[BUFF_SIZE];
+	//counter
 	int i;
 	stringArray stringArray;
 	int bestDistanceFound = INT_MAX;
 
-	
-	
-
+	//read all the info from the input textfile into the struct stringArray
 	stringArray = testGetFile(stringsToRead);
 
+	//malloc memory for the best string found
 	char *result = malloc(stringArray.stringLength * sizeof(char));
 	double tempEnd = (double)stringArray.stringLength;
 	double hex = (double)16;
@@ -90,23 +80,19 @@ int main(int argc, char **argv)
 	// insert your code here(!)
 	if (myrank == 0)
 	{
-
-		
+		//malloc memory for the stringlenght + 1 (distance) * number of processors
 		char *bestResults = malloc(sizeof(char) * (stringArray.stringLength+1) * nprocs);
 
+		//region that every processor have to go through
 		regionSizePerProc = endRegion / nprocs;
-		//printf("Regio : %f EndRegio : %f\n", regionSizePerProc, endRegion);
+		
 		double regionSizePerProcCeil = ceil(regionSizePerProc);
+		//start the time
 		begin = clock();
+		//go through the region that processor 0 is responsible for
 		result = generateString((double)0, regionSizePerProcCeil, stringArray, &bestDistanceFound);
 
-		//printf("%s\n\n", result);
-		//printf("%d\n", bestDistanceFound);
-
 		char *buffer = malloc((stringArray.stringLength + 1) * sizeof(char));
-		snprintf(buffer, sizeof(buffer)+1, "%s%d", result, bestDistanceFound);
-		//printf("%d\n\n", bestDistanceFound);
-		//printf("%s\n", buffer);
 		memcpy(bestResults, buffer, stringArray.stringLength+1);
 
 
@@ -121,18 +107,12 @@ int main(int argc, char **argv)
 			
 			
 		}
-		//printf("%s", bestResults);
-		//printf("%s", bestResults);
-		//printf("%s\n", bestResults);
-		
-
+	
 		int bestDistanceFinal = INT_MAX, positionOfBestString = 0;
 
 		for (int k = 1; k <= nprocs; k++) {
 		
 			int distanceTemp = *(bestResults + stringArray.stringLength * k + (k-1)) -48;
-			//printf("%d\n\n", distanceTemp);
-			//printf("%s", bestResults);
 			if (distanceTemp < bestDistanceFinal) {
 			
 				bestDistanceFinal = distanceTemp;
@@ -149,8 +129,10 @@ int main(int argc, char **argv)
 
 		float t_ges = end - begin;
 		t_ges = t_ges / CLOCKS_PER_SEC;
+		//printTimeToTxt(t_ges, nprocs, stringArray.stringLength);
+		//TODO: write result into a text file
+		//		with number of procs and string lenght
 
-		//printf("%d",bestDistanceFinal);
 		if (verbosity == 1) {
 
 			printResults(bestDistanceFinal, bestStringFinal, stringArray);
@@ -167,73 +149,24 @@ int main(int argc, char **argv)
 			printResults(bestDistanceFinal, bestStringFinal, stringArray);
 
 		}
-		//printf("%s", bestStringFinal);
-		//printf("Dist = %d K= %d", bestDistanceFinal,positionOfBestString);
-		//printResults(bestDistanceFinal, bestStringFinal, stringArray);
-
 	}
 	else 
 	{
-		//char *test = "1234562";
-
+		
 		regionSizePerProc = endRegion / nprocs;
 		
 		double regionSizePerProcCeil = ceil(regionSizePerProc);
 		double regionSizePerProcFloor = floor(regionSizePerProc);
 		startRegion = regionSizePerProcFloor * myrank;
-		//printf("Regio: %f EndRegio : %f\n", regionSizePerProc, startRegion);
-
 		result = generateString(startRegion, startRegion + regionSizePerProcCeil, stringArray, &bestDistanceFound);
 		char *buffer = malloc(stringArray.stringLength + 1 * sizeof(char));
 		snprintf(buffer, sizeof(buffer)+1, "%s%d", result,bestDistanceFound);
-		//printf("%s\n\n", buffer);
 	
 		MPI_Send(buffer, stringArray.stringLength + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
-		//printf("Here is my Size %d and Length %d", sizeLength[0], sizeLength[1]);
 	
 	}
 	MPI_Finalize();
-	
 
-	//read .txt file
-	
-
-
-	
-
-
-	//
-	//begin = clock();
-	//result = generateString(startRegion, endRegion, stringArray, &bestDistanceFound);
-	//end = clock();
-
-	//float t_ges = end - begin;
-	//t_ges = t_ges/CLOCKS_PER_SEC;
-	//
-	//
-	//
-	//
-
-	//if (verbosity == 1) {
-	//
-	//	printResults(bestDistanceFound, result, stringArray);
-	//
-	//}
-	//else if (verbosity == 2) {
-	//
-	//	printf("The program took %f seconds to finish!\n", t_ges);
-
-	//}
-	//else if (verbosity == 3) {
-	//
-	//	printf("The program took %f seconds to finish!\n\n\n", t_ges);
-	//	printResults(bestDistanceFound, result, stringArray);
-	//
-	//}
-	//
-	//system("pause");
-	////Finalize the MPI Environment
-	//
 	return 0;
 
 }
@@ -272,37 +205,36 @@ stringArray testGetFile(int stringsToRead) {
 			stringArrayTemp.stringSize = stringsToRead;
 
 		}
-		//printf("Size: %s\n", line);
+		
 
 	}
 
-	//stringArray = malloc(arraySize * sizeof * stringArray);
+	
 	//read the second line - write it into the arrayLenght
 	if (fgets(line, len, fp) != NULL) {
 
 		stringArrayTemp.stringLength = atoi(line);
-		//printf("Lenght: %s\n", line);
+		
 
 	}
 	free(line);
 	line = malloc(stringArrayTemp.stringLength * sizeof * stringArrayTemp.data);
-	//printf("Content of Array: \n\n");
+	
 	int count = 0;
 	stringArrayTemp.data = malloc(stringArrayTemp.stringSize * sizeof * stringArrayTemp.data);
 	while (fgets(line, len, fp) != NULL) {
-		//printf("%s", line);
+		
 		int len = strlen(line); //where buff is your char array fgets is using
 		if (line[len - 1] == '\n')
 			line[len - 1] = '\0';
 		char *persistString = malloc(sizeof(char) * stringArrayTemp.stringLength);
 		memcpy(persistString, line, sizeof(line));
 		stringArrayTemp.data[count] = persistString;
-		//printf("%s\n", stringArrayTemp.data[0]);
+		
 		count = count + 1;
 	}
 	free(line);
-	//printf("\n");
-
+	
 	int *sizeLength = malloc(2 * sizeof * sizeLength);
 	sizeLength[0] = stringArrayTemp.stringSize;
 	sizeLength[1] = stringArrayTemp.stringLength;
@@ -379,3 +311,22 @@ void printResults(int distance, char *closestString, stringArray stringArrayComp
 
 }
 
+void printTimeToTxt(float tges, int nprocs, int stringLenght)
+{
+	FILE *fp = fopen("timeresults.txt", "a");
+	if (fp == NULL)
+	{
+		printf("Error opening file!\n");
+		exit(1);
+	}
+
+	int maxStringLenght = stringLenght;
+	int numberOfProcs = nprocs;
+	float timeGes = tges;
+	fprintf(fp, "\n\n");
+	fprintf(fp, "Number Of Cores: %d\n", numberOfProcs);
+	fprintf(fp, "Time needed: %f seconds \n", timeGes);
+	fprintf(fp, "Maximum String Lenght: %d\n", maxStringLenght);
+
+	//fclose(fp);
+}
